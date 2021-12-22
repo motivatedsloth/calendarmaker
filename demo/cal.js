b95986e177ddfb9cffd54a93c45730dc695a4273
Alan Buss <al@constellationwebservices.com>
2021-12-22 17:24:40 -0600

    added demo

---
+++ demo/cal.js
@@ -0,0 +1,6 @@
+import calendarmaker from "../src/calendarmaker.js";
+
+calendarmaker.month("#thismonth");
+calendarmaker.year("#thisyear");
+calendarmaker.week("#thisweek");
+calendarmaker.agenda("#thisagenda");
