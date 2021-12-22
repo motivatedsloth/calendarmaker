f9dba06c3ebc7f1e4f2b9f7c3750710778e73722
Alan Buss <al@constellationwebservices.com>
2021-12-22 17:16:45 -0600

    update main

--- src/calendarmaker.js
+++ src/calendarmaker.js
@@ -8,45 +8,37 @@
  * https://github.com/motivatedsloth/calendarmaker/blob/master/LICENSE
  *
  */
-import "./calendarmaker.css";
-
 import defOptions from "./defaults.js";
 import calendar from "./calendar.js";
 import templates from "./templates.js";
 
-export function year(el, opts = {}, temps = {}){
+let calendarmaker;
+
+function year(el, opts = {}, temps = {}){
     let o = Object.assign({}, defOptions, opts, {"unit": "year"}), //options
         t = Object.assign({}, templates.year, temps); //templates
     return new calendar(getElement(el), o, t);
 }
 
-/**
- * create calendar object to display months 
- *
- * @param Element el
- * @param Object opts 
- *
- * @return display 
- */
-export function month(el, opts = {}, temps = {}){
+function month(el, opts = {}, temps = {}){
     let o = Object.assign({}, defOptions, opts, {"unit": "month"}), //options
         t = Object.assign({}, templates.month, temps); //templates
     return new calendar(getElement(el), o, t);
 }
 
-export function week(el, opts = {}, temps = {}){
+function week(el, opts = {}, temps = {}){
     let o = Object.assign({}, defOptions, opts, {"unit": "week"}), //options
         t = Object.assign({}, templates.week, temps); //templates
     return new calendar(getElement(el), o, t);
 }
 
-export function agenda(el, opts = {}, temps = {}){
+function agenda(el, opts = {}, temps = {}){
     let o = Object.assign({}, defOptions, opts, {"unit": "agenda", "controls": false}), //options
         t = Object.assign({}, templates.agenda, temps); //templates
     return new calendar(getElement(el), o, t);
 }
 
-export function datepicker(el, opts, temps = {}){
+function datepicker(el, opts, temps = {}){
     let o = Object.assign({}, defOptions, opts, {"unit": "month"}), //options
         t = Object.assign({}, templates.datepicker, temps); //templates
     return new calendar(getElement(el), o, t);
@@ -58,3 +50,14 @@ function getElement(el){
     }
     return el;
 }
+ 
+
+calendarmaker = {
+    "month": month,
+    "week": week,
+    "agenda": agenda,
+    "datepicker": datepicker,
+    "year": year
+}
+
+export default calendarmaker;
